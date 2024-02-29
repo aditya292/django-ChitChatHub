7711869761b9dca98413cb1799c7d4349bccfdf4
Aditya Nishad <adityanishad292@gmail.com>
2024-02-29 21:52:45 +0530

    First commit

---
+++ base/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from base.models import Room,Message,Topic,User
+
+# Register your models here.
+
+admin.site.register(Room)
+admin.site.register(Message)
+admin.site.register(Topic)
+admin.site.register(User)
+
+
+
